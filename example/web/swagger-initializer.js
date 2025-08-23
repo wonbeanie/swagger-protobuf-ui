9d48f04d4d317ae346b39c417b17c453a28004c8
wonbeanie <goobb2580@gmail.com>
2025-08-23 17:01:18 +0900

    fix(example/web): 스웨거 UI 번들 URL 옵션 수정
    URL 설정이 webpack-dev-server(localhost:8080)과 맞지 않아
    스웨거 설정 파일을 찾을 수 없는 버그를 고쳤습니다.

--- example/web/swagger-initializer.js
+++ example/web/swagger-initializer.js
@@ -1,6 +1,6 @@
 window.onload = function() {
 	window.ui = SwaggerProtoBufUIBundle(MyProto,{
-		url : "http://127.0.0.1:5500/server.json",
+		url : "http://localhost:8080/server.json",
 		dom_id: '#swagger-ui',
 		presets: [
 			SwaggerUIBundle.presets.apis,
