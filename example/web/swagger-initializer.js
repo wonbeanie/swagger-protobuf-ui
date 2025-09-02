1fe98214111c1422276414dbcb0f920c5a12fbc6
wonbeanie <goobb2580@gmail.com>
2025-09-02 15:25:01 +0900

    chore(example/web): 예시 URL 포트를 8080으로 고정
    테스트 환경의 일관성을 위해 예제 서버의 URL 포트를 8080으로 고정합니다.

--- example/web/swagger-initializer.js
+++ example/web/swagger-initializer.js
@@ -1,6 +1,6 @@
 window.onload = function() {
 	window.ui = SwaggerProtoBufUIBundle(MyProto,{
-		url : "http://localhost:5500/server.json",
+		url : "http://localhost:8080/server.json",
 		dom_id: '#swagger-ui'
 	});
 };
