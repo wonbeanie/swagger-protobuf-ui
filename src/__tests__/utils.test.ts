2884da2c983c26b5d11218eb7c8655b2c4291102
wonbeanie <goobb2580@gmail.com>
2025-09-13 02:23:50 +0900

    build(jest): jest 환경 구성
    프로젝트의 코드 안정성과 테스트를 위해 Jest를 도입하였습니다.
    
    - 'jest' 관련 패키지를 설치했습니다.
    - 'jestconfig.js' 파일을 추가했습니다.
    - 'tsconfig.json'에서 'esModuleInterop' : true를 추가했습니다.
    - 'package.json'에 'npm run test("jest")'을 추가하였습니다.
    - utils.ts의 테스트 코드인 'utils.test.ts'를 추가하였습니다.

---
+++ src/__tests__/utils.test.ts
@@ -0,0 +1,10 @@
+import { isBlank } from "../utils";
+
+describe('Utils.ts 테스트', () => {
+   it('isBlank 테스트', () => {
+      expect(isBlank(' ')).toBe(true);
+      expect(isBlank('test')).toBe(false);
+      expect(isBlank('test ')).toBe(false);
+      expect(isBlank(' test')).toBe(false);
+   });
+});
