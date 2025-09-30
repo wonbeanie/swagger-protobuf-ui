c086c853f2cba3ca12d385e8598c8ebf1c90d7f0
wonbeanie <goobb2580@gmail.com>
2025-09-30 16:37:17 +0900

    build(playwright): e2e 테스트 서버 명령어 추가
    - e2e 테스트를 위한 서버(example/server, web)명령어를 추가하였습니다.
    - 서버 동시 실행을 위해 'concurrently'를 추가하였습니다.
    - playwright 설정 파일에서 서버 command를 'npm run e2e:start-server'로 수정하였습니다.

--- playwright.config.ts
+++ playwright.config.ts
@@ -76,7 +76,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run start:test',
+    command: 'npm run e2e:start-server',
     url: 'http://localhost:8080',
     reuseExistingServer: !process.env.CI,
   },
